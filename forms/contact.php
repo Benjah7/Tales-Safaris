287d0198ec3b8086249e3f24aa5a31a80333fadd
benjah <benjaminmutava@gmail.com>
2024-08-07 14:46:36 +0300

    email spam protectionset up

--- forms/contact.php
+++ forms/contact.php
@@ -24,18 +24,23 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
+
+<div class="form-group d-none">
+<input type="text" class="form-control" name="first_name">
+<div class="validate"></div>
+</div>
 
   echo $contact->send();
 ?>
--- forms/newsletter.php
+++ forms/newsletter.php
@@ -6,7 +6,6 @@
   
   */
 
-  // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'talessafariskenya@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
@@ -24,14 +23,14 @@
   $contact->subject ="New Subscription: " . $_POST['email'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['email'], 'Email');
 
